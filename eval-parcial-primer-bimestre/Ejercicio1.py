7e3c9e10f43b279703862f5d9ed328ad49e359c9
Paul Sanchez <paxasaval1003@gmail.com>
2018-04-27 15:52:35 -0500

    Ejercicio1 y 2

---
+++ eval-parcial-primer-bimestre/Ejercicio1.py
@@ -0,0 +1,8 @@
+def main():
+	
+	longitud=eval(input("Ingrese la longitud de la habitacion"))
+	ancho=eval(input("Ingrese el ancho de la habitacion"))
+	superficie=longitud*ancho
+	print("La superficie de la habitacion es de: %.2f" %superficie)
+	
+	
